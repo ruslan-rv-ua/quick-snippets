3647c0272a0259390ab58b8d7fddcd44eefb007b
Ruslan Iskov <ruslan.rv.ua+github@gmail.com>
2026-02-27 09:04:44 +0200

    fix(accessibility): disable browser autocomplete and suggestions on snippet title inputs

--- src/components/EditSnippetModal.tsx
+++ src/components/EditSnippetModal.tsx
@@ -109,6 +109,9 @@ export function EditSnippetModal({
           maxLength={50}
           value={title}
           onChange={(e) => setTitle(e.target.value)}
+          autoComplete="off"
+          aria-autocomplete="none"
+          spellCheck={false}
           aria-invalid={submitted && !!errors.title ? 'true' : undefined}
           aria-describedby={submitted && errors.title ? 'edit-title-err' : undefined}
         />
